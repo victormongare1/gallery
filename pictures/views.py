269457c2badaf45ae0d64b23accd41111abaf407
victormongare1 <vicmongz254@gmail.com>
2020-05-22 14:01:30 +0300

    created template file to display pictures

--- pictures/views.py
+++ pictures/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http  import HttpResponse,Http404
 
 # Create your views here.
+
+def allPictures():
+    '''
+    view function to display all pictures in the application
+    '''
+    pictures=
+    return render(request, 'index.html', {"pictures":pictures})
