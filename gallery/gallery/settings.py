3c7c5b849768cdbbe108f4438e77768f20d9b3a9
victormongare1 <vicmongz254@gmail.com>
2020-05-22 12:53:30 +0300

    added bootstrap

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pictures.apps.PicturesConfig',
+    'bootstrap3',
+
 ]
 
 MIDDLEWARE = [
